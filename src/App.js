f94a66073e94572104aa47622dbba9269d76220c
Maddi Cook <maddihcook@gmail.com>
2023-12-19 05:57:28 -0600

    created form elements

--- src/App.js
+++ src/App.js
@@ -19,8 +19,14 @@ function Logo() {
 }
 
 function Form() {
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 💖</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
@@ -31,7 +37,7 @@ function Form() {
       </select>
       <input type="text" placeholder="Item..." />
       <button></button>
-    </div>
+    </form>
   );
 }
 
